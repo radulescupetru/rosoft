3e552f3d8888a625cd2323656ba729f0be8e1e22
Radulescu Petru <petruradulescu.v@gmail.com>
2018-03-22 16:32:34 +0200

    Initial commit

---
+++ first.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 16:05:05 2018
+
+@author: peter
+
+"""
+import cv2
+from keras import Sequential
+import tornado
+
+def main():
+    print("asda")
+
+if __name__=="__main__":
+    main()
