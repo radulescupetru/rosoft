b57e528dc3090dc808fcaf56a64e72c0c82a0a3e
Radulescu Petru <petruradulescu.v@gmail.com>
2018-03-22 17:34:44 +0200

    Created server

--- first.py
+++ first.py
@@ -6,11 +6,31 @@ Created on Thu Mar 22 16:05:05 2018
 
 """
 import cv2
-from keras import Sequential
-import tornado
+from pathlib import Path
+import tornado.web
+import tornado.ioloop
 
-def main():
-    print("asda")
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('templates/index.html')
+    
+    def post(self):
+        text = self.get_body_argument("arg")
+        self.redirect("/")
+        
+        file_name = Path(r"D:\projects\rosoft\rosoft\text.txt")
+        with file_name.open('a+') as f:
+            f.write(text+'\n')
+
+def make_app():
+    return tornado.web.Application([
+            (r"/",MainHandler),
+            #(r"/logs",LogHandler),
+            ])
 
 if __name__=="__main__":
-    main()
+   app = make_app()
+   app.listen(8080)
+   tornado.ioloop.IOLoop.current().start()
